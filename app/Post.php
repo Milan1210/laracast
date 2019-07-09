3f64cf9df3241e6c9f52ba1ec68f91d8c6b1f0b0
Milan1210 <milan99191@live.com>
2019-07-09 15:27:40 +0200

    equelement

--- app/Post.php
+++ app/Post.php
@@ -2,12 +2,10 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
-
 class Post extends Model
 {
-    protected $fillable = [
-        'title',
-        'content',
-    ];
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,4 +4,10 @@
    <h1>{{$post->title}}</h1> 
    <h3>{{$post->content}}</h3> 
    <small>{{$post->created_at->toFormattedDateString()}}</small>
+
+   @foreach ($post->comments as $coment)
+       <article>
+       {{$coment->body}}
+       </article>
+   @endforeach
 @endsection
