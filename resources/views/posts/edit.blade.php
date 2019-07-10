34bccfe706216e83999af27c704a62917e5eeeca
Milan1210 <milan99191@live.com>
2019-07-10 14:53:13 +0200

    edit and delete

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -2,16 +2,17 @@
 
 @section('content')
     <h1>Create new form</h1>
-    <form method="post" action="{{route('posts.store')}}">
+    <form method="post" action="{{route('posts.update')}}">
         @csrf
+            <input type="hidden" id="idp" name="idp" value={{$posts->id}}>
         <div class="form-group">
           <label for="title">Title</label>
-          <input type="text" class="form-control" id="title" name="title" placeholder="Title">
+          <input type="text" class="form-control" id="title" name="title" value="{{$posts->title}}">
         </div>
 
         <div class="form-group">
           <label for="exampleInputPassword1">Content</label>
-         <textarea name="content" id="content" cols="30" rows="10" class="form-control"></textarea>
+         <textarea name="content" id="content" cols="30" rows="10" class="form-control" >{{$posts->content}}</textarea>
         </div>
         
         <div class="checkbox">
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,11 +5,15 @@
     <tr>
       <th>Title</th>
       <th>COntent</th>
+      <th>Edit</th>
+      <th>Delete</th>
     </tr>
     @foreach ($posts as $post)
     <tr>
       <td><a href="post/{{$post->id}}">{{$post->title}}</a></td>
       <td>{{$post->content}}</td>
+      <td><a href="{{route('posts.edit',$post->id)}}" class="btn btn-success">Edit</a></td>
+      <td><a href="{{route('posts.delete',$post->id)}}" class="btn btn-danger">Delete</a></td>
     </tr>
     @endforeach
   </table>
